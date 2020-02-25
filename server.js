812b246919d7b75f1d3b944377a207a50f83c13f
remcopasma <rempiepasma@gmail.com>
2020-02-25 11:03:11 +0100

    add query params

--- server.js
+++ server.js
@@ -1,12 +1,21 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
     res.send('Mijn dating server!');
 });
-
+// Route home
+app.get('/home', function(req, res) {
+    res.sendFile(path.join(__dirname+'/public/index.html'));
+});
+// Query params
+app.get('/home/name/:Naam/gender/:Gender', function (req, res) {
+    res.send(req.params)
+  })
+// Show 404 
 app.use(function(req, res){
     res.type('text/plain')
     res.status(404)

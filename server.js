983d25264f97ab6c3f9c0cc50d29038f37f6c649
remcopasma <rempiepasma@gmail.com>
2020-02-27 14:14:21 +0100

    started working with ejs

--- server.js
+++ server.js
@@ -4,17 +4,22 @@ const path = require('path');
 
 app.use(express.static('public'));
 
-app.get('/', (req, res) => {
-    res.send('Mijn dating server!');
-});
+// Using ejs
+app.set("view engine", "ejs");
+app.get("/", (req, res) => res.render("pages/index"));
+
 // Route home
 app.get('/home', function(req, res) {
     res.sendFile(path.join(__dirname+'/public/index.html'));
 });
-// Query params
-app.get('/home/name/:Naam/gender/:Gender', function (req, res) {
-    res.send(req.params)
-  })
+
+
+// // Query params
+// app.get('/home/name/:Naam/gender/:Gender', function (req, res) {
+//     res.send(req.params)
+//     console.log(req.params);
+//   })
+
 // Show 404 
 app.use(function(req, res){
     res.type('text/plain')
